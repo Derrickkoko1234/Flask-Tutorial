99b2ba42ef5dc37263d25956070307d5441db5be
Derrick Koko <kokoderrick3@gmail.com>
2022-05-23 08:39:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/home/<string:name>')
-def hello(name):
-    return "Hello, " + name
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home/<int:id>')
+def hello(id):
+    return "Hello, " + str(id)
+
+@app.route('/onlyget', methods=['GET'])
+def get_req():
+    return 'You can only get this web page.'
 
 if __name__ == "__main__":
     app.run(debug=True)
